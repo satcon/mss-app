1456ea8774b040b06f51e7b011d83d8cf649b88a
kevin <satcon@163.com>
2015-10-26 22:18:21 +0800

    add 404 page

--- mss-web-server/src/main/java/com/jd/mss/controller/IndexController.java
+++ mss-web-server/src/main/java/com/jd/mss/controller/IndexController.java
@@ -1,32 +1,24 @@
 package com.jd.mss.controller;
 
-import java.util.List;
-import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.alibaba.fastjson.JSON;
-import com.mss.model.SysMenu;
-import com.mss.service.SysMenuService;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
 	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
-	
-	private SysMenuService sysMenuService;
+
 	
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String index() {
 		logger.info("index首页");
 		try {
-			Integer count = sysMenuService.selectMenuCount();
-			List<SysMenu> list = sysMenuService.findAllMenus();
-			
-			logger.info("【菜单】：{}", JSON.toJSONString(list));
-			logger.info("【菜单数量】：{}", count);
 			
 		} catch (Exception e) {
 			logger.error("【异常】：" ,e);
@@ -35,13 +27,12 @@ public class IndexController {
 		
 		return "layout/layout";
 	}
-
-	public SysMenuService getSysMenuService() {
-		return sysMenuService;
-	}
-
-	@Resource
-	public void setSysMenuService(SysMenuService sysMenuService) {
-		this.sysMenuService = sysMenuService;
+	
+	
+	@RequestMapping("/error/notFound")
+	public String notFound(HttpServletRequest request) {
+		logger.info("【404页面】:" + request.getRequestURI());
+		
+		return "error/404";
 	}
 }
